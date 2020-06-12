41f461ebd431d1748c6ac311f10a89c44ced87a9
PonyPC <a@nide8.com>
2020-06-12 17:19:33 +0800

    Add files via upload

--- _onekey-decompile-apk.bat
+++ _onekey-decompile-apk.bat
@@ -1,7 +1,7 @@
 @echo off
 echo ==================================
 echo Onekey Decompile Apk
-echo v2.4.0 20200612
+echo v2.0.2 20200612
 echo Based on https://code.google.com/p/onekey-decompile-apk/
 echo **********************************
 echo How to use
@@ -19,11 +19,8 @@ cd /d "%~dp1"
 rem delete previous generation
 rd /s /q "%~dpn1" >NUL 2>NUL
 
-rem install framework to ensure decompile without errors
-echo .........framework........
-java -jar "_tools\apktool\apktool_2.4.1.jar" if "_tools\apktool\framework-res.apk" -p "%~dp0_tools"
 echo .........apktool..........
-java -jar "_tools\apktool\apktool_2.4.1.jar" d "%apkFile%"
+java -jar "_tools\apktool\apktool_2.4.1.jar" d "%apkFile%" --frame-path "%~dp0_tools\apktool"
 echo .........dex2jar..........
 call _tools\dex2jar\d2j-dex2jar -f "%apkFile%" -o "%jarFile%"
 echo .........jd-gui...........
